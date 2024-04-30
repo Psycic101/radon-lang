b2f1273f106482e4a180269c59d487fa937d6dc0
Psycic101 <simonurban101@gmail.com>
2024-04-30 22:19:52 +0100

    Add project files.

---
+++ radon-lang/ErrorCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace radon_lang
+{
+    internal class ErrorCodes
+    {
+        internal const int ERROR_FILE_NOT_FOUND = 0x2;
+        internal const int ERROR_BAD_ARGUMENTS = 0xA0;
+    }
+}
---
+++ radon-lang/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using radon_lang;
+
+Console.WriteLine("Hello, World!");
+
+string sourceFile = (args.Length == 0) ? string.Empty : args[0];
+
+if (string.IsNullOrWhiteSpace(sourceFile))
+{
+    return ErrorCodes.ERROR_BAD_ARGUMENTS;
+}
+
+if (!File.Exists(sourceFile))
+{
+    return ErrorCodes.ERROR_FILE_NOT_FOUND;
+}
+
+using (StreamReader sr = new(sourceFile))
+{
+    StringBuilder sb = new();
+
+    while (!sr.EndOfStream)
+    {
+        int currChar = sr.Read();
+        Console.WriteLine(currChar.ToString());
+        sb.Append((char)currChar);
+    }
+
+    Console.WriteLine(sb.ToString());
+}
+
+return 0;
